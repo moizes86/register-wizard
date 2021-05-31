11f2ab635affce134d93d93f7cdba98799263df3
moizes86 <moshe.mn86@gmail.com>
2021-05-31 13:50:35 +0300

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import InputGroup from "react-bootstrap/InputGroup";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import "./App.css";
+
+function App() {
+  return <div className="App">
+    <Form>
+      
+    </Form>
+  </div>;
+}
+
+export default App;

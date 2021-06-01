9eb256bb062cb97e3fd11fc1bd6dc9ee78309a2f
moizes86 <moshe.mn86@gmail.com>
2021-06-01 10:23:42 +0300

    pulled from master

--- src/components/CustomForm.jsx
+++ src/components/CustomForm.jsx
@@ -1,14 +1,9 @@
 import React, { useState } from "react";
-import ErrorMessage from './ErrorMessages';
 
 import Button from "react-bootstrap/Button";
-import ButtonGroup from "react-bootstrap/ButtonGroup";
-import InputGroup from "react-bootstrap/InputGroup";
 import Form from "react-bootstrap/Form";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
-import ErrorMessages from "./ErrorMessages";
+import NameEmailBirthday from "./name-email-birthday/NameEmailBirthday";
 
 const CustomForm = () => {
   const [loginData, setLoginData] = useState({
@@ -31,7 +26,7 @@ const CustomForm = () => {
     },
 
     birthday: {
-      value: null,
+      value: "",
       errors: [],
       validations: {
         required: true,
@@ -78,9 +73,9 @@ const CustomForm = () => {
     },
   });
 
+  const [isSection2, setIsSection2] = useState(false)
+
   const validateInput = ({ target: { value, name } }) => {
-      console.log(name);
-      console.log(value);
     const newErrors = [];
     const { validations } = loginData[name];
 
@@ -99,76 +94,21 @@ const CustomForm = () => {
         value: value,
         errors: newErrors,
       },
+      
     });
   };
 
   return (
     <Container>
       <Form>
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridName">
-            <Form.Label>Name</Form.Label>
-            <Form.Control type="text" name="name" placeholder="Enter name" onBlur={validateInput} />
-            <ErrorMessages errors={loginData.name.errors} />
-          </Form.Group>
+        <NameEmailBirthday
+          loginData={loginData}
+          setLoginData={setLoginData}
+          validateInput={validateInput}
+          setIsSection2={setIsSection2}
+        />
 
-          <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-              type="email"
-              name="email"
-              placeholder="Enter email"
-              onBlur={validateInput}
-            />
-            <ErrorMessages errors={loginData.email.errors} />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formGridBirthday">
-            <Form.Label>Birthday</Form.Label>
-            <Form.Control
-              type="date"
-              name="birthday"
-              onBlur={validateInput}
-            />
-            <ErrorMessages errors={loginData.birthday.errors} />
-          </Form.Group>
-        </Row>
-
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridCity">
-            <Form.Label>City</Form.Label>
-            <Form.Control />
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridState">
-            <Form.Label>State</Form.Label>
-            <Form.Control as="select" defaultValue="Choose...">
-              <option>Choose...</option>
-              <option>...</option>
-            </Form.Control>
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridZip">
-            <Form.Label>Zip</Form.Label>
-            <Form.Control />
-          </Form.Group>
-        </Row>
-
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-        </Row>
-
-        <Form.Group className="mb-3" id="formGridCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
+        {isSection2 && <p>Hey</p>}
 
         <Button variant="primary" type="submit">
           Submit

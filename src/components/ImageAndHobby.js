03a69c22ed601199f490797e2a30a2bb95374dc5
dorliani <ddorliani@gmail.com>
2021-06-01 16:14:34 +0300

    styling

--- src/components/ImageAndHobby.js
+++ src/components/ImageAndHobby.js
@@ -2,47 +2,55 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import { Link } from "react-router-dom";
 
 const ImageAndHobby = ({ validateInput }) => {
   return (
     <>
-      <Row className="mb-3">
-        <Col>
-          <Form.Group as={Col} controlId="formGridImage">
-            <Form.Label>Image</Form.Label>
-            <Form.Control
-              name="image"
-              type="text"
-              placeholder="Enter image url"
-              onBlur={validateInput}
-            />
-          </Form.Group>
-        </Col>
+      <Card className="text-center">
+        <Card.Header>Register Wizard</Card.Header>
+        <Card.Body>
+          <Row className="mb-2">
+            <Col>
+              <Form.Group as={Col} controlId="formGridImage">
+                <Form.Label>Image</Form.Label>
+                <Form.Control
+                  name="image"
+                  type="text"
+                  placeholder="Enter image url"
+                  onBlur={validateInput}
+                />
+              </Form.Group>
+            </Col>
 
-        <Col>
-          <Form.Group as={Col} controlId="formGridHobby">
-            <Form.Label>Hobby</Form.Label>
-            <Form.Control
-              name="hobby"
-              type="text"
-              placeholder="Enter Hobby"
-              onBlur={validateInput}
-            />
-          </Form.Group>
-        </Col>
-      </Row>
-
-      <Row className="mb-3">
-        <Col md={1}>
-          <Link exact to="/cityStreetNumber">
-            <Button variant="primary" type="submit">
-              Previous
-            </Button>
-          </Link>
-        </Col>
-      </Row>
+            <Col>
+              <Form.Group as={Col} controlId="formGridHobby">
+                <Form.Label>Hobby</Form.Label>
+                <Form.Control
+                  name="hobby"
+                  type="text"
+                  placeholder="Enter Hobby"
+                  onBlur={validateInput}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row className="mb-2">
+            <Col md={4}>
+              <Link exact to="/cityStreetNumber">
+                <Button variant="primary" type="submit">
+                  Previous
+                </Button>
+              </Link>
+            </Col>
+          </Row>
+        </Card.Body>
+        <Card.Footer className="text-muted">
+          Moshe, Jacob and Dor are in the house
+        </Card.Footer>
+      </Card>
     </>
   );
 };
